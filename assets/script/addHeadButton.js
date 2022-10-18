c037ee14761beaf5d29100b31d6b06a388d960a6
Imam Mujaddid <mueiya.whatever@gmail.com>
2022-10-19 05:40:29 +0700

    Initial commit

---
+++ assets/script/addHeadButton.js
@@ -0,0 +1,8 @@
+function popUp (val) {
+    const addButton = document.getElementById(val);
+    addButton.style.display = 'block';
+}
+function popUpHide (val) {
+    const addButton = document.getElementById(val);
+    addButton.style.display = 'none';
+}
---
+++ assets/script/inputOverlay.js
@@ -0,0 +1,8 @@
+const container = document.getElementsByClassName('inputBook')
+
+function overlayOn() {
+    document.getElementsById('addBookOverlay').style.display = 'flex';
+}
+function overlayOff () {
+    document.getElementsById('addBookOverlay').style.display = 'none';
+}
